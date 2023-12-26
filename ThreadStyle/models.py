8419cc1814d4809f614a07921604e41b03b3a5c2
Lokesh <sailoki789@gmail.com>
2023-12-26 20:50:01 +0530

    changed models for multiselection

--- ThreadStyle/models.py
+++ ThreadStyle/models.py
@@ -1,9 +1,49 @@
 from django.db import models
+from django import forms
 
-# Create your models here.
-# models.py
+from django.db import models
+from django import forms
 
-#base class
+class MultiSelectField(models.Field):
+    def __init__(self, *args, **kwargs):
+        self.max_length = kwargs.pop('max_length', None)
+        kwargs['blank'] = True
+        kwargs['null'] = True
+        super(MultiSelectField, self).__init__(*args, **kwargs)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super(MultiSelectField, self).deconstruct()
+        kwargs['max_length'] = self.max_length
+        return name, path, args, kwargs
+
+    def from_db_value(self, value, expression, connection):
+        if value is None:
+            return value
+        return value.split(',')
+
+    def to_python(self, value):
+        if isinstance(value, list):
+            return value
+        elif value is None:
+            return value
+        return value.split(',')
+
+    def get_prep_value(self, value):
+        if value is None:
+            return value
+        return ','.join(value)
+
+    def formfield(self, **kwargs):
+        defaults = {
+            'form_class': forms.MultipleChoiceField,
+            'choices': self.choices,
+            'widget': forms.CheckboxSelectMultiple,
+        }
+        defaults.update(kwargs)
+        return super().formfield(**defaults)
+
+    def db_type(self, connection):
+        return 'text'
 class Product(models.Model):
     SIZE_CHOICES = [
         ('XS', 'Extra Small'),
@@ -29,13 +69,20 @@ class Product(models.Model):
         ('hand', 'Hand Wash'),
     ]
 
-    name = models.CharField(max_length=255) 
-    image=models.ImageField(upload_to='pics',default=None)
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='pics', default=None)
     original_price = models.DecimalField(max_digits=10, decimal_places=2)
     offer_price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-    available_sizes = models.CharField(max_length=10, choices=SIZE_CHOICES, blank=True, null=True)
-    color = models.CharField(max_length=255)
+    available_sizes = MultiSelectField(choices=SIZE_CHOICES)
+    color = MultiSelectField(choices=[
+        ('black', 'Black'),
+        ('green', 'Green'),
+        ('blue', 'Blue'),
+        ('pink', 'Pink'),
+        ('white', 'White'),
+        ('yellow', 'Yellow'),
+    ])
     material = models.CharField(max_length=255)
     fit = models.CharField(max_length=10, choices=FIT_CHOICES)
     sleeve = models.CharField(max_length=10, choices=SLEEVE_CHOICES)
@@ -47,7 +94,6 @@ class Product(models.Model):
     class Meta:
         abstract = True
 
-#Inheriting base class
 class MenTshirt(Product):
     pass
 
@@ -58,5 +104,4 @@ class MenOversizedTshirt(Product):
     pass
 
 class WomenOversizedTshirt(Product):
-    pass  
-
+    pass
