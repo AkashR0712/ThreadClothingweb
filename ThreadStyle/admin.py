e4b284872f44cac4a946f556e79e9282f56dac5c
Lokesh <sailoki789@gmail.com>
2023-12-26 17:37:57 +0530

    admin and settings

--- ThreadStyle/admin.py
+++ ThreadStyle/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import MenTshirt, WomenTshirt, MenOversizedTshirt, WomenOversizedTshirt
+
+admin.site.register(MenTshirt)
+admin.site.register(WomenTshirt)
+admin.site.register(MenOversizedTshirt)
+admin.site.register(WomenOversizedTshirt)
